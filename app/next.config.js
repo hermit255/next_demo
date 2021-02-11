e8e6862f8f8da966b7035d643602cf732c4f80e8
hermit255 <hermit255@gmail.com>
2021-02-11 16:59:53 +0900

    mod

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,5 @@
-require("dotenv").config();
 const repository_name = "next_demo";
 
 module.exports = {
-  //assetPrefix: process.env.GITHUB_PAGES ? "/" + repository_name : "./",
   assetPrefix: "/" + repository_name,
 };
