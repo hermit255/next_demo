ed9be9759dd9cccd1fd38e2bbfbe13689d10215d
hermit255 <hermit255@gmail.com>
2021-02-12 08:42:04 +0900

    mod

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,12 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
 const repository_name = "next_demo";
 
 module.exports = (phase) => {
-  const assetPrefix = phase === "build" ? "/" + repository_name : "/";
+  const assetPrefix =
+    phase === PHASE_PRODUCTION_BUILD ? "/" + repository_name : "/";
   return {
     assetPrefix: assetPrefix,
   };
