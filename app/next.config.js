c257b11c0e54d6d18c335e37e491ed29a8150d94
hermit255 <hermit255@gmail.com>
2021-02-11 16:57:43 +0900

    mod

--- app/next.config.js
+++ app/next.config.js
@@ -3,5 +3,5 @@ const repository_name = "next_demo";
 
 module.exports = {
   //assetPrefix: process.env.GITHUB_PAGES ? "/" + repository_name : "./",
-  assetPrefix: "/next_demo",
+  assetPrefix: "/" + repository_name,
 };
