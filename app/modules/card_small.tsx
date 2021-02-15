7d70c4215232dc75565023889a4306977bd01c36
hermit255 <hermit255@gmail.com>
2021-02-16 01:25:17 +0900

    Added landing page.

---
+++ app/modules/card_small.tsx
@@ -0,0 +1,21 @@
+import card_s from 'styles/CardSmall.module.css'
+type Card = {
+  title: string,
+  text: string,
+  image_path: string,
+};
+
+export default function CardSmall(card: Card) {
+  return (
+    <div className="card w-100 zoom">
+      <img className="card-img-top" src={card.image_path} alt="Card image"></img>
+      <div className="card-body">
+        <h5 className="card-title"> {card.title} </h5>
+        <p className="card-text"> {card.text} </p>
+        <p className="text-center">
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </p>
+      </div>
+    </div>
+  )
+}
