8817be8f1b4e918d510ab1d54452fb2f5225db0e
hermit255 <hermit255@gmail.com>
2021-02-16 01:23:06 +0900

    Added bootstrap.

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 docker-compose run app npx create-next-app .
 docker-compose run app npm install --save-dev typescript @types/react @types/node
 docker-compose run app npm i --save -D dotenv
+docker-compose run app npm i --save bootstrap
 touch tsconfig.json
