c52b758c71f927ff37cecd22e07196486c5f4230
hermit255 <hermit255@gmail.com>
2021-03-15 20:42:31 +0900

    Added libraries.

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 docker-compose run app npx create-next-app .
 docker-compose run app npm install --save-dev typescript @types/react @types/node
 docker-compose run app npm i --save -D dotenv
+docker-compose run app npm i react-bootstrap bootstrap
+docker-compose run app npm i axios
+docker-compose run app npm i @types/xml2js xml2js
 touch tsconfig.json
