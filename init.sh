0bcdd9fbe53816f3815147bac1435bf13e01408c
hermit255 <hermit255@gmail.com>
2021-04-21 16:24:45 +0900

    Added tailwindcss.

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 docker-compose run app npx create-next-app .
-docker-compose run app npm install --save-dev typescript @types/react @types/node
-docker-compose run app npm i --save -D dotenv
+docker-compose run app npm install -D typescript @types/react @types/node
+docker-compose run app npm i -D dotenv fork-ts-checker-webpack-plugin
+# install tailwindcss
+docker-compose run app npm i tailwindcss@latest postcss@latest autoprefixer@latest
+npx tailwindcss init -p
 touch tsconfig.json
